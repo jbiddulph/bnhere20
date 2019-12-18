7eebd979aa32aa39653ce9846e3c306dbde23fee
John Biddulph <johnbiddulph@johns-mbp-2.home>
2019-12-18 22:39:20 +0000

    routes setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {WelcomeComponent} from "./welcome/welcome.component";
-import {SignupComponent} from "./auth/signup/signup.component";
-import {LoginComponent} from "./auth/login/login.component";
-import {ArticlesComponent} from "./admin/articles/articles.component";
-import {AuthGuard} from "./auth/auth.guard";
+import {WelcomeComponent} from './welcome/welcome.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {LoginComponent} from './auth/login/login.component';
+import {ArticlesComponent} from './admin/articles/articles.component';
+import {AuthGuard} from './auth/auth.guard';
+import {RequestResetComponent} from './auth/password/request-reset/request-reset.component';
+import {ResponseResetComponent} from './auth/password/response-reset/response-reset.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'request-password-reset', component: RequestResetComponent },
+  { path: 'response-password-reset', component: ResponseResetComponent },
   { path: 'articles', component: ArticlesComponent, canActivate: [AuthGuard] },
 ];
 
